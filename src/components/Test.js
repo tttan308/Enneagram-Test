6eaa9a733f576bf983e5101864fbc003f7355133
TanTran <tantran.300803@gmail.com>
2022-10-01 15:21:57 +0700

    change question type from json into js

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,14 +3,8 @@ import { useState } from "react";
 import { questions } from '../utils/questions' 
  
 function Test() {
-  const ordered = Object.keys("order").sort().reduce(
-    (obj, key) => { 
-      obj[key] = questions[key]; 
-      return obj;
-    }, 
-    {}
-  );
-  console.log(ordered)
+  questions.sort((a,b)=>(a.order>b.order)?1:-1);
+  console.log(questions)
   return (
     <div className="p-[500px] w-[600px] h-[400px] justify-self-center mt-40 px-3 py-1 border-2 rounded-lg bg-[#252d4a]">
       <div className="grid overflow-hidden grid-cols-2 grid-rows-2 gap-2 text-white">
--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,182 +1,182 @@
-export const questions = {
-    0: {
+export const questions = [
+    {
         question: "I can feel irritated for no special reason.",
         type: 1,
         order: Math.random()
     },
-    1: {
+    {
         question: "No one’s more critical of me than I am of myself.  ",
         type: 1,
         order: Math.random()
     },
-    2: {
+    {
         question: "Getting the job done right is more important than getting the job done fast.",
         type: 1,
         order: Math.random()
     },
-    3: {
+    {
         question: "As a child, I was more serious and realistic than many other children.",
         type: 1,
         order: Math.random()
     },
-    4: {
+    {
         question: "It’s hard not to cry when I feel sentimental, even when I’m in public.",
         type: 2,
         order: Math.random()
     },
-    5: {
+    {
         question: "I hate asking for help, even though I like giving it. ",
         type: 2,
         order: Math.random()
     },
-    6: {
+    {
         question: "I find it hard to hold back when it comes to offering help and advice.",
         type: 2,
         order: Math.random()
     },
-    7: {
+    {
         question: "I take pride in the way I put people at ease.",
         type: 2,
         order: Math.random()
     },
-    8: {
+    {
         question: "My desire to achieve knows no bounds.",
         type: 3,
         order: Math.random()
     },
-    9: {
+    {
         question: "I like the idea of involving my intimate partner in my work life, so we don't drift apart.",
         type: 3,
         order: Math.random()
     },
-    10: {
+    {
         question: "Meeting my deadline matters more than getting every tiny detail just right.",
         type: 3,
         order: Math.random()
     },
-    11: {
+    {
         question: "Being a winner makes my efforts worthwhile.",
         type: 3,
         order: Math.random()
     },
-    12: {
+    {
         question: "Even when I seem accepted by a group, it's hard to feel that I really belong.",
         type: 4,
         order: Math.random()
     },
-    13: {
+    {
         question: "I often feel a sense of longing without really knowing why.",
         type: 4,
         order: Math.random()
     },
-    14: {
+    {
         question: "I can embrace the sweet side of sorrow.",
         type: 4,
         order: Math.random()
     },
-    15: {
+    {
         question: "Emotionally intense situations make me feel really alive. ",
         type: 4,
         order: Math.random()
     },
-    16: {
+    {
         question: "I'm sometimes seen by others as socially unresponsive.",
         type: 5,
         order: Math.random()
     },
-    17: {
+    {
         question: "Having a private space is a necessity, not a luxury. ",
         type: 5,
         order: Math.random()
     },
-    18: {
+    {
         question: "I prefer to thoroughly think through a problem before I act. ",
         type: 5,
         order: Math.random()
     },
-    19: {
+    {
         question: "I like to read up in advance before trying some new activity.",
         type: 5,
         order: Math.random()
     },
-    20: {
+    {
         question: "Maintaining close ties with friends and family helps me feel safe in a chaotic world.",
         type: 6,
         order: Math.random()
     },
-    21: {
+    {
         question: "My feelings of apprehension are less intense when I have a project to work on.",
         type: 6,
         order: Math.random()
     },
-    22: {
+    {
         question: "Making other people laugh helps me feel calmer and less anxious. ",
         type: 6,
         order: Math.random()
     },
-    23: {
+    {
         question: "Learning to assert myself gives me confidence and lessens my anxiety.",
         type: 6,
         order: Math.random()
     },
-    24: {
+    {
         question: "I feel an urge to escape when I land in situations that require an emotional commitment.",
         type: 7,
         order: Math.random()
     },
-    25: {
+    {
         question: "My desire to think positive can sometimes be like an addiction.",
         type: 7,
         order: Math.random()
     },
-    26: {
+    {
         question: "I like to do something new, not the same old thing.  ",
         type: 7,
         order: Math.random()
     },
-    27: {
+    {
         question: "I like to do so many things that it's easy to spread myself too thin.",
         type: 7,
         order: Math.random()
     },
-    28: {
+    {
         question: "I can both make the rules and enforce them.",
         type: 8,
         order: Math.random()
     },
-    29: {
+    {
         question: "I'm a force to be reckoned with.",
         type: 8,
         order: Math.random()
     },
-    30: {
+    {
         question: "My bluntness can sometimes blow people away.",
         type: 8,
         order: Math.random()
     },
-    31: {
+    {
         question: "People say I have a powerful aura.",
         type: 8,
         order: Math.random()
     },
-    32: {
+    {
         question: "I can be stubborn in a way that avoids direct confrontation.",
         type: 9,
         order: Math.random()
     },
-    33: {
+    {
         question: "Most people see me as more peaceful than I really am.",
         type: 9,
         order: Math.random()
     },  
-    34: {
+    {
         question: "Being in nature is deeply soothing to me.",
         type: 9,
         order: Math.random()
     },  
-    35: {
+    {
         question: "I can get cranky when things get too complex.",
         type: 9,
         order: Math.random()
     }
-}
+]
